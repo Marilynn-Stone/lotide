ca4987ffb021adf4c3180f25f6202e4dd2dbc759
Marilynn Stone <hello@marilynnstone.com>
2022-05-22 20:08:11 +0000

    create final version

--- tail.js
+++ tail.js
@@ -8,8 +8,19 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(array) {
   return array.slice(1);
-}
+};
 
 const result = tail(["hello", "lighthouse", "labs"]);
 assertEqual(result.length, 2);
-assertEqual(result[0], )
+assertEqual(result[0], "lighthouse");
+assertEqual(result[1], "labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+const oneWord = tail(["lift"]);
+assertEqual(oneWord.length, 0);
+
+const emptyArray = tail([]);
+assertEqual(emptyArray.length, 0);
