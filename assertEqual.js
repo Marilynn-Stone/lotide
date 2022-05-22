3ab35b33eff48b48230a173a05fccf04c05bc50d
Marilynn Stone <hello@marilynnstone.com>
2022-05-22 16:33:51 +0000

    create assertion function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("summer", "summer");
+assertEqual(7,11);
