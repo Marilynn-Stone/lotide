c53ef46b297fbcf59388b040bda17661edcce878
Marilynn Stone <hello@marilynnstone.com>
2022-05-28 20:34:58 +0000

    create function to determine index of letter

--- letterPositions.js
+++ letterPositions.js
@@ -10,16 +10,27 @@ const assertArraysEqual = function(array1, array2) {
   return console.log(`🟢 Assertion Passed: ${array1} === ${array2}`);
 };
 
-//loop through string
+// loop through string
 //for each character, return what index it is found at
 //ignore white space
 
 const letterPositions = function(sentence) {
   const results = {};
-  let spacesRemoved = sentence.split(" ").join("");
-  console.log(spacesRemoved);
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " ") {
+      continue;
+    } else {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
   return results;
 };
 
 letterPositions("lighthouse in the house");
-//assertArraysEqual(letterPositions("hello").e, [1]);
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("lighthouse in the house").g, [2]);
