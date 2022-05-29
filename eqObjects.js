47de8c2eafde825f5c3f81fd409a0ca179e6ad2b
Marilynn Stone <hello@marilynnstone.com>
2022-05-29 16:42:32 +0000

    refactor code with mentor

--- eqObjects.js
+++ eqObjects.js
@@ -25,17 +25,13 @@ const eqObjects = function(object1, object2) {
   const object2Keys = Object.keys(object2);
   if (object1Keys.length !== object2Keys.length) {
     return false;
-  } else {
-    for (let key of object1Keys) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (eqArrays(object1[key], object2[key])) {
-          return true;
-        } else {
-          if (object1[key] !== object2[key]) {
-            return false;
-          }
-        }
-      }
+  }
+  for (let key of object1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    }
+    if (object1[key] !== object2[key]) {
+      return false;
     }
   }
   return true;
