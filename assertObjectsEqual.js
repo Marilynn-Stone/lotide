d38282533469e96932ba3fe7c0f97cf0f0a60c4b
Marilynn Stone <hello@marilynnstone.com>
2022-05-29 16:52:38 +0000

    create final versioni

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -12,25 +12,24 @@ const eqArrays = function(array1, array2) {
 
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   const actualKeys = Object.keys(actual);
   const expectedKeys = Object.keys(expected);
   if (actualKeys.length !== expectedKeys.length) {
-    return console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    for (let key of actualKeys) {
-      if (Array.isArray(actual[key]) && Array.isArray(expected[key])) {
-        if (eqArrays(actual[key], expected[key])) {
-          return console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-        } else {
-          if (actual[key] !== expected[key]) {
-            return console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-          }
-        }
+    return console.log(`🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+  for (let key of actualKeys) {
+    if (Array.isArray(actual[key]) && Array.isArray(expected[key])) {
+      if (eqArrays(actual[key], expected[key])) {
+        return console.log(`🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+      }
+      if (actual[key] !== expected[key]) {
+        return console.log(`🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
       }
     }
   }
-  return true;
+  return console.log(`🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
 };
 
 assertObjectsEqual({a:1, b:2}, {a:1, b:2});
-assertObjectsEqual({a:1, b:2}, {c:3, d:4});
+assertObjectsEqual({a:1, b:2}, {c:3, d:4, e:4});
