0055560ccc973ebb36f5a138439ff42ad45488ad
Marilynn Stone <hello@marilynnstone.com>
2022-06-11 02:07:54 +0000

    refactor to remove test code

--- tail.js
+++ tail.js
@@ -1,26 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
 
-const result = tail(["hello", "lighthouse", "labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "lighthouse");
-assertEqual(result[1], "labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const oneWord = tail(["lift"]);
-assertEqual(oneWord.length, 0);
-
-const emptyArray = tail([]);
-assertEqual(emptyArray.length, 0);
+module.exports = tail;
