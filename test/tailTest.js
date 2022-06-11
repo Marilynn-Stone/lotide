5bfedfca1586be59f98bdac05a5549092283793b
Marilynn Stone <hello@marilynnstone.com>
2022-06-11 16:36:54 +0000

    convert test file to use M & C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-//test code
-
-const result = tail(["hello", "lighthouse", "labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "lighthouse");
-assertEqual(result[1], "labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const oneWord = tail(["lift"]);
-assertEqual(oneWord.length, 0);
-
-const emptyArray = tail([]);
-assertEqual(emptyArray.length, 0);
+describe("#tail", () => {
+  const array = ["hello", "lighthouse", "labs"];
+  const result = tail(array);
+  const oneWord = tail(["lift"]);
+  const emptyArray = tail([]);
+  it('returns new array length of 2 for ["hello", "lighthouse", "labs"]', () => {
+    assert.deepEqual(result.length, 2);
+  });
+  it('returns index 0 as "lighthouse" for ["hello", "lighthouse", "labs"]', () => {
+    assert.deepEqual(result[0], "lighthouse");
+  });
+  it('returns original array length of 3 for ["hello", "lighthouse", "labs"]', () => {
+    assert.deepEqual(array.length, 3);
+  });
+  it('returns new array length of 0 for ["lift"]', () => {
+    assert.deepEqual(oneWord.length, 0);
+  });
+  it('returns new array length of 0 for []', () => {
+    assert.deepEqual(emptyArray.length, 0);
+  });
+});
