e07834acb3d6c81909931a54be8eb084003cc9c9
Marilynn Stone <hello@marilynnstone.com>
2022-06-13 01:55:37 +0000

    refactor test to require eqArrays

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,16 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
-// FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   const actualKeys = Object.keys(actual);
